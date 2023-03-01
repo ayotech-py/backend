ae26a31cbe0af42205c78222d39c6a99aa05aae0
ayotech-py <ajaomalik2018@gmail.com>
2023-03-01 21:47:07 +0100

    backend

--- LetsQuiz_Api/views.py
+++ LetsQuiz_Api/views.py
@@ -124,7 +124,7 @@ class GetSecuredData(APIView):
     def get(self, request):
         user_id = request.user.id
         quiz_details = OrganizeQuiz.objects.filter(organiser_id=user_id).values(
-            'quiz_title', 'subject', 'quiz_id', 'created_at')
+            'quiz_title', 'subject', 'quiz_id', 'created_at', 'past')
         username = User.objects.get(id=user_id)
 
         context = {
