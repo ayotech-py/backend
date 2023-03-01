27dd6dbdcd0c5775b49c187b54effa4773e8c5b0
ayotech-py <ajaomalik2018@gmail.com>
2023-03-01 22:18:55 +0100

    backend

--- LetsQuiz_Api/views.py
+++ LetsQuiz_Api/views.py
@@ -201,10 +201,8 @@ class QuizStatus(APIView):
         data = json.loads(request.body)
         quiz_status = data['status']
         query = OrganizeQuiz.objects.get(quiz_id=quiz_id)
-        query.status = quiz_status
         query.past = quiz_status
-        print(quiz_status)
-        print(query.past)
+        query.status = quiz_status
 
         query.save(update_fields=['status'])
 
